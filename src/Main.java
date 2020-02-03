e290441a586aa8872ac24fd9bc06a3147ab10740
Akash <akashsinghkld@gmail.com>
2020-02-03 15:10:51 +0530

    Changed the structure of the code.

--- src/Main.java
+++ src/Main.java
@@ -5,28 +5,49 @@ public class Main {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter the 1st value: " );
-        float one = scan.nextFloat();
+        String one = scan.nextLine();
         System.out.print("Enter the 2nd value: " );
-        float two = scan.nextFloat();
-        System.out.print("Choos an operation (/ * + -): " );
-        int s = scan.nextInt(), answer = 0;
+        String two = scan.nextLine();
+        System.out.print("Choose an operation (/ * + -): " );
+        String s = scan.next();
+        double answer = 0;
         switch (s){
-            case 1:
-                answer  = (int) (one/two);
+            case "/":
+                answer  = divideValues(one,two);
                 break;
-            case 2:
-                answer  = (int) (one*two);
+            case "*":
+                answer  = multiplyValues(one,two);
                 break;
-            case 3:
-                answer  = (int) (one+two);
+            case "+":
+                answer  = addValues(one,two);
                 break;
-            case 4:
-                answer  = (int) (one-two);
+            case "-":
+                answer  = subtractValues(one,two);
                 break;
             default:
                 throw new IllegalStateException("Unexpected value: " + s);
         }
 
         System.out.println("The answer is: " +answer);
+    }
+    private static double addValues(String one, String two) {
+        double d1 = Double.parseDouble(one);
+        double d2 = Double.parseDouble(two);
+        return d1 + d2;
+    }
+    private static double multiplyValues(String one, String two) {
+        double d1 = Double.parseDouble(one);
+        double d2 = Double.parseDouble(two);
+        return d1 * d2;
+    }
+    private static double divideValues(String one, String two) {
+        double d1 = Double.parseDouble(one);
+        double d2 = Double.parseDouble(two);
+        return d1 / d2;
+    }
+    private static double subtractValues(String one, String two){
+        double d1 = Double.parseDouble(one);
+        double d2 = Double.parseDouble(two);
+        return d1-d2;
     }
 }
